be60d54e9985fb21e5c34918a1213438296bedfc
nicknggt <nickng99@protonmail.com>
2021-10-15 17:01:48 +1100

    Update Unbanker.cs

--- Functions/Unbanker.cs
+++ Functions/Unbanker.cs
@@ -5,10 +5,12 @@ using System.Collections.Generic;
 using System.Windows.Forms;
 
 public class Script {
-	public ScriptInterface Bot;
+	
+    public ScriptInterface bot => ScriptInterface.Instance;
+
 
 	public void ScriptMain(ScriptInterface bot){
-		Bot = bot;
+		
 
 		// You can do this
 		string[] requiredItems = {"Dragon Claw",
@@ -41,21 +43,21 @@ public class Script {
 		while (Bot.Player.State == 2) {}
 
 		// Loads bank
-		Bot.Player.LoadBank();
+		bot.Player.LoadBank();
 
 		// Declares AC items to bank
 		List<string> Whitelisted = new List<string>(){"Note", "Item", "Resource", "QuestItem", "ServerUse"};
 
 		// Banks unneeded items that are included in the whitelisted
-		foreach(var item in Bot.Inventory.Items) {
+		foreach(var item in bot.Inventory.Items) {
 			if (!Whitelisted.Contains(item.Category.ToString())) continue;
-			if (item.Name != "Treasure Potion" && item.Coins && !Array.Exists(Items, x => x == item.Name)) Bot.Inventory.ToBank(item.Name);
+			if (item.Name != "Treasure Potion" && item.Coins && !Array.Exists(Items, x => x == item.Name)) bot.Inventory.ToBank(item.Name);
 		}
 
 		// Unbanks the required items
 		foreach (var item in Items) {
-			if (Bot.Bank.Contains(item)) Bot.Bank.ToInventory(item);
-			if (AddToDrops) Bot.Drops.Add(item);
+			if (bot.Bank.Contains(item)) bot.Bank.ToInventory(item);
+			if (AddToDrops) bot.Drops.Add(item);
 		}
 
 	}
