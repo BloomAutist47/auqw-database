fcf037bb87f1fbe5ba0f3382609fbf7854f11db2
BloomAutist47 <BloomAutist47@users.noreply.github.com>
2021-11-10 14:14:49 +0800

    Added GameCall.cs. It is List of useful flash calls not included in rbot yet

---
+++ GameCalls.cs
@@ -0,0 +1,62 @@
+/// <filename>: GameCalls.cs
+/// <author>: Bloom 
+/// <description>: List of useful flash calls not included in rbot yet
+
+
+using RBot;
+
+
+public class Testingclass {
+    public ScriptInterface bot => ScriptInterface.Instance;
+
+    public void ScriptMain(ScriptInterface bot) {
+
+
+      HideMonsters(true);
+      ShowFPSCounter(true);
+    
+    }
+
+   /// <summary>
+   /// Hides the monsters for performance
+   /// </summary>
+   /// <param name="Value"> true -> hides monsters. false -> reveals them </param>
+   public void HideMonsters(bool Value) {
+      switch(Value) {
+         case true:
+            if (!bot.GetGameObject<bool>("ui.monsterIcon.redX.visible")) {
+               bot.CallGameFunction("world.toggleMonsters");
+            }
+            return;
+         case false:
+            if (bot.GetGameObject<bool>("ui.monsterIcon.redX.visible")) {
+               bot.CallGameFunction("world.toggleMonsters");
+            }
+            return;
+
+      }
+
+   }
+
+   /// <summary>
+   /// Shows FPS counter
+   /// </summary>
+   /// <param name="Value"> true -> hides fps. false -> reveals them </param>
+   public void ShowFPSCounter(bool Value) {
+      switch(Value) {
+         case true:
+            if (!bot.GetGameObject<bool>("ui.mcFPS.visible")) {
+               bot.CallGameFunction("world.toggleFPS");
+            }
+            return;
+         case false:
+            if (bot.GetGameObject<bool>("ui.mcFPS.visible")) {
+               bot.CallGameFunction("world.toggleFPS");
+            }
+            return;
+      }
+
+
+   }
+
+}
